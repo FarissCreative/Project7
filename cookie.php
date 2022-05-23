7a5a0d0d9a7bc1032484cfb8687ce8bf128e6188
FarissCreative <83574304+FarissCreative@users.noreply.github.com>
2022-05-23 14:02:55 +0200

    update file
    get access to the demo live version for the cookie.php file

--- cookie.php
+++ cookie.php
@@ -8,21 +8,23 @@ body {
   margin: auto;
   margin-top: 250px;
 }
+
 </style>
 </head>
 <body>
   <h1>Hotel Villa Gulla </h1>
+  <hr>
 
   <?php
 $returning_visitor = false;
 //makes a cookie user ( with a limit in a week calculated in seconds)
 if (!isset($_COOKIE['bruger'])) {
-	setcookie('bruger', 'Nadin', time() + 7 * 86400);
+	setcookie('bruger', 'examtest', time() + 7 * 86400);
 } else {
 	$returning_visitor = true;
 }
 ?>
-  
+
 
 <h2> 
  <?php
@@ -33,6 +35,10 @@ echo $returning_visitor ? 'Byder dig velkommen tilbage!, klar til at book endnu 
 </h2>
 
 <p><strong>Note:</strong> Du skal muligvis genindlaese siden for at display velkomst teksten basseret paa om man er foerstegang besøgende paa siden eller har besoegt foe.</p>
+
+<hr>
+<p><strong>Watch demo:</strong> http://fariss.dk/cookies.php</p>
+
 
 </body>
 </html>
